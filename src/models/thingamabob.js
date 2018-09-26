8263b509a6ed9a98f422c3b579b022cf956b3c17
R Wood <archaurore@gmail.com>
2018-09-26 14:23:56 +0200

    created a basic model

---
+++ src/models/thingamabob.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const thingamabobSchema = mongoose.Schema({
+    awesome_field : {
+        type : String,
+        default : null
+    }
+});
+
+const Thingamabob = mongoose.model('Thingamabob', thingamabobSchema);
+
+module.exports = {Thingamabob};
